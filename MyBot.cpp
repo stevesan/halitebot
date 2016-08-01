1e1d51eb4a77d287024da8674b81598506a33a17
stevesan <stevenan@gmail.com>
2016-07-31 21:46:44 -0400

    utility bsaed method, but it sucks

--- MyBot.cpp
+++ MyBot.cpp
@@ -9,6 +9,56 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 
+typedef int utility;
+
+template<typename MAP> utility evalUtil(unsigned char myId, const MAP& map)
+{
+    // first, count how many i have vs. other players (non-0)
+    unsigned int player2terr[MAX_PLAYERS];
+    memset(player2terr, 0, sizeof(player2terr[0]) * MAX_PLAYERS);
+
+    FORMAP(map, x, y) {
+        hlt::Site site = map.getSite({x,y});
+        if( site.owner != 0 ) {
+            player2terr[site.owner]++;
+        }
+    }
+
+    unsigned int bestPlayer = -1;
+    for( int i = 1; i < MAX_PLAYERS; i++ ) {
+        if( bestPlayer == -1 || player2terr[i] > player2terr[bestPlayer] ) {
+            bestPlayer = i;
+        }
+    }
+
+    bool catchUp = false;
+    const int lagTiles = 2;
+    if( bestPlayer != myId && player2terr[bestPlayer] > player2terr[myId] + lagTiles ) {
+        catchUp = true;
+    }
+
+
+    int terrUtil = catchUp ? 256 : 20;
+    utility util = 0;
+    FORMAP(map, x, y) {
+        hlt::Site site = map.getSite({x,y});
+        if( site.owner == myId ) {
+            util += terrUtil + site.strength;
+        }
+    }
+    return util;
+}
+
+std::string DIR2STR[] = { "Still", "North", "East", "South", "West" };
+
+void output_moveset( std::ofstream& os, const hlt::MoveSet& moves ) {
+    for(auto move = moves.begin(); move != moves.end(); ++move) {
+        auto l = move->loc;
+        auto d = move->dir;
+        os << l.x << ", " << l.y << " : " << DIR2STR[d] << std::endl;
+    }
+}
+
 int main() {
 	srand(time(NULL));
 
@@ -16,26 +66,52 @@ int main() {
 
 	std::cout.sync_with_stdio(0);
 
-	unsigned char myID;
+    hlt::PlayerId myId;
 	hlt::GameMap presentMap;
-	getInit(myID, presentMap);
+	getInit(myId, presentMap);
 	sendInit("TheDarkness");
 
-	std::set<hlt::Move> moves;
+    unsigned int evalsPerFrame = 100;
+    std::vector< hlt::MoveSet > movesets(evalsPerFrame);
+	hlt::GameMap workMap(presentMap);
+
+    int frameCount = 0;
+
 	while(true) {
-		moves.clear();
 
-		getFrame(presentMap);
+        dbg << "-- frame " << frameCount << std::endl;
+         
+        getFrame(presentMap);
+        int bestset = -1;
+        utility bestutil = 0;
 
-		for(unsigned short a = 0; a < presentMap.height; a++) {
-			for(unsigned short b = 0; b < presentMap.width; b++) {
-				if (presentMap.getSite({ b, a }).owner == myID) {
-					moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
-				}
-			}
-		}
+        for( int i = 0; i < evalsPerFrame; i++ ) {
+            hlt::MoveSet& moves = movesets[i];
+            moves.clear();
 
-		sendFrame(moves);
+            FORMAP(presentMap, x, y) {
+                hlt::Location u = {x,y};
+                hlt::Site src = presentMap.getSite(u);
+                if (src.owner == myId) {
+                    unsigned char d = (unsigned char)(rand() % 5);
+                    moves.insert({ u, d });
+                }
+            }
+
+            presentMap.copyInto(workMap);
+            workMap.simMoves(moves, myId);
+            utility util = evalUtil(myId, workMap);
+
+            if(util > bestutil || bestset == -1) {
+                bestutil = util;
+                bestset = i;
+            }
+        }
+
+        dbg << "using moveset " << bestset << " with util " << bestutil << std::endl;
+        output_moveset(dbg, movesets[bestset]);
+        sendFrame(movesets[bestset]);
+        frameCount++;
 	}
 
 	return 0;
--- hlt.hpp
+++ hlt.hpp
@@ -14,7 +14,15 @@
 const int DIRECTIONS[] = {STILL, NORTH, EAST, SOUTH, WEST};
 const int CARDINALS[] = {NORTH, EAST, SOUTH, WEST};
 
+#define MAX_PLAYERS 16
+
+#define FORMAP(map, x, y)  \
+		for(unsigned short x = 0; x < (map).width; x++) \
+			for(unsigned short y = 0; y < (map).height; y++) \
+
 namespace hlt{
+    typedef unsigned char PlayerId;
+
 	struct Location{
 		unsigned short x, y;
 	};
@@ -22,8 +30,18 @@ namespace hlt{
 		return ((l1.x + l1.y)*((unsigned int)l1.x + l1.y + 1) / 2) + l1.y < ((l2.x + l2.y)*((unsigned int)l2.x + l2.y + 1) / 2) + l2.y;
 	}
 
+	struct Move{
+		Location loc; unsigned char dir;
+	};
+	static bool operator<(const Move& m1, const Move& m2) {
+		unsigned int l1Prod = ((m1.loc.x + m1.loc.y)*((unsigned int)m1.loc.x + m1.loc.y + 1) / 2) + m1.loc.y, l2Prod = ((m2.loc.x + m2.loc.y)*((unsigned int)m2.loc.x + m2.loc.y + 1) / 2) + m2.loc.y;
+		return ((l1Prod + m1.dir)*(l1Prod + m1.dir + 1) / 2) + m1.dir < ((l2Prod + m2.dir)*(l2Prod + m2.dir + 1) / 2) + m2.dir;
+	}
+
+    typedef std::set<hlt::Move> MoveSet;
+
 	struct Site{
-		unsigned char owner;
+		PlayerId owner;
 		unsigned char strength;
 		unsigned char production;
 	};
@@ -67,7 +85,7 @@ namespace hlt{
 			return atan2(dy, dx);
 		}
 
-		Location getLocation(Location l, unsigned char direction) {
+		Location getLocation(Location l, unsigned char direction) const {
 			if(direction != STILL) {
 				if(direction == NORTH) {
 					if(l.y == 0) l.y = height - 1;
@@ -88,19 +106,91 @@ namespace hlt{
 			}
 			return l;
 		}
-		Site& getSite(Location l, unsigned char direction = STILL) {
+		const Site& getSite(Location l, unsigned char direction = STILL) const {
 			l = getLocation(l, direction);
 			return contents[l.y][l.x];
 		}
-	};
 
-	struct Move{
-		Location loc; unsigned char dir;
+        void setSite(Location l, Site site) {
+            contents[l.y][l.x] = site;
+        }
+
+        void setSite(Location l, unsigned char direction, Site site) {
+            l = getLocation(l, direction);
+            contents[l.y][l.x] = site;
+        }
+
+        void copyInto(GameMap& dest)  const {
+            FORMAP( (*this), i, j ) {
+                Location l = {i,j};
+                dest.setSite( l, getSite(l) );
+            }
+        }
+
+        bool simMoves(const MoveSet& moves, PlayerId id) {
+            for(auto move = moves.begin(); move != moves.end(); ++move) {
+                auto loc = move->loc;
+                auto dir = move->dir;
+                Site src = getSite(loc);
+                if(src.owner != id) {
+                    return false;
+                }
+
+                if( dir == STILL ) {
+                    src.strength += src.production;
+                    setSite(loc, src);
+                }
+                else {
+                    Site dest = getSite(loc, dir);
+                    if(dest.owner == id) {
+                        // combine
+                        dest.strength = (unsigned char)
+                            std::min((int)255, (int)dest.strength + (int)src.strength);
+                        src.strength = 0;
+                    }
+                    else {
+                        // combat
+                        int ds = dest.strength;
+                        int ss = src.strength;
+                        dest.strength = (unsigned char)std::max(0, (int)dest.strength - ss);
+                        src.strength = (unsigned char)std::max(0, (int)src.strength - ds);
+
+                        if( src.strength == 0 && dest.strength == 0 ) {
+                            // both die
+                            src.owner = 0;
+                            dest.owner = 0;
+                        }
+                        else {
+                            if( src.strength == 0 ) {
+                                // lose..
+                                src.owner = 0;
+                            }
+                            else if( dest.strength == 0 ) {
+                                // win!
+                                // do move
+                                dest.owner = id;
+                                dest.strength = src.strength;
+                                src.strength = 0;
+                            }
+                            else {
+                                // neither is dead
+                                // owners do not change
+                            }
+                        }
+
+                        dest.owner = id;
+                        dest.strength = src.strength;
+                        src.strength = 0;
+                    }
+
+                    // apply mods
+                    setSite(loc, src);
+                    setSite(loc, dir, dest);
+                }
+            }
+            return true;
+        }
 	};
-	static bool operator<(const Move& m1, const Move& m2) {
-		unsigned int l1Prod = ((m1.loc.x + m1.loc.y)*((unsigned int)m1.loc.x + m1.loc.y + 1) / 2) + m1.loc.y, l2Prod = ((m2.loc.x + m2.loc.y)*((unsigned int)m2.loc.x + m2.loc.y + 1) / 2) + m2.loc.y;
-		return ((l1Prod + m1.dir)*(l1Prod + m1.dir + 1) / 2) + m1.dir < ((l2Prod + m2.dir)*(l2Prod + m2.dir + 1) / 2) + m2.dir;
-	}
 }
 
 #endif
